78b03d2346082c5be281a885475d53d4d17ef7c4
Vitali Matusevich <vimatu@gmail.com>
2020-02-24 14:44:45 +0300

    refact: change structure the task - count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,4 @@
 module.exports = function countCats(matrix) {
-  // throw 'Not implemented';
-  // remove line with error and write your code here
-  // console.log(matrix);
   return matrix.flat().reduce((acc, elm) => {
     if (elm === '^^') return acc += 1;
     return acc; 
