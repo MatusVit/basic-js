0b738f6ed6ac2729b19d41cead4d666ea117b5d2
Vitali Matusevich <matvi@mail.ru>
2020-02-24 22:12:19 +0300

    feat: solved the task - carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,14 +2,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-
-  // console.log(`${sampleActivity} - ${typeof sampleActivity}`);
-  
-  if (((typeof sampleActivity) === 'string') 
-        && Number.isFinite(+sampleActivity)
-        && +sampleActivity < MODERN_ACTIVITY
-        && +sampleActivity > 0) {
-    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD / 0.693)
+  const number = parseFloat(sampleActivity);
+  if (typeof sampleActivity === 'string' 
+      && number
+      && number < MODERN_ACTIVITY
+      && number > 0) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / number) * HALF_LIFE_PERIOD / 0.693)
   }
   return false;
 };
