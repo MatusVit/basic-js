48454cbfbf4cec803650884455bea24cdece27eb
Vitali Matusevich <vimatu@gmail.com>
2020-02-24 14:34:51 +0300

    feat: solved the task - count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(matrix) {
+  // throw 'Not implemented';
   // remove line with error and write your code here
+  // console.log(matrix);
+  return matrix.flat().reduce((acc, elm) => {
+    if (elm === '^^') return acc += 1;
+    return acc; 
+  }, 0)
 };
