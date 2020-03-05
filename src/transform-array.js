6d36413990297e0b3a9aff47fd134ff69bd5795c
Vitali Matusevich <vimatu@gmail.com>
2020-03-05 12:33:42 +0300

    feat: solved the task - transform-array not all test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,40 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error();
+    let transformArray = arr.reduce((acc, elm, index, array) => {
+        if (elm === '--double-next') {
+            if (array[index + 1]) acc.push(array[index + 1]);
+        } else {
+            acc.push(elm);
+        }
+        return acc;
+    }, []);
+
+    transformArray = transformArray.reduce((acc, elm, index, array) => {
+        if (elm === '--double-prev') {
+            if (array[index - 1]) acc.push(array[index - 1]);
+        } else {
+            acc.push(elm);
+        }
+        return acc;
+    }, []);
+   
+    transformArray = transformArray.reduce((acc, elm, index, array) => {
+        if (elm === '--discard-prev') {
+            if (array[index - 1]) acc.slice(index - 1, 1);
+        } else {
+            acc.push(elm);
+        }
+        return acc;
+    }, []);
+
+    transformArray = transformArray.reduce((acc, elm, index, array) => {
+        if (elm === '--discard-next') {
+            if (array[index + 1]) array.slice(index + 1, 1);
+        } else {
+            acc.push(elm);
+        }
+        return acc;
+    }, []);
+
+    return transformArray;
+}
