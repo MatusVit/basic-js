dcbb27ae3275a2b277e416214f0ee97f3f87e6ba
Vitali Matusevich <vimatu@gmail.com>
2020-03-10 17:55:13 +0300

    feat: solving the task - vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,34 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct = true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        if (!str || !key) throw new Error();
+        const arrEncode = str.toUpperCase().split('');
+        const arrKey = key.toUpperCase().split('').map((elm) => elm.charCodeAt() - 65);
+
+        let keyArrayPosition = 0;
+        const resultArray = arrEncode.map((elm) => {
+            let code = elm.charCodeAt();
+            if (code < 64 || code > 90) return elm;
+            let cipherCode = code + arrKey[keyArrayPosition];
+            if (cipherCode > 90) cipherCode = cipherCode - 26;
+
+            keyArrayPosition += 1;
+            if (keyArrayPosition === arrKey.length) keyArrayPosition = 0;
+
+            return String.fromCharCode(cipherCode);
+        });
+
+        return resultArray.join('');
+    }
+
+    decrypt(strDecode, strKey) {
+        if (!strDecode || !strKey) throw new Error();
+
+
+        return '';
     }
 }
 
